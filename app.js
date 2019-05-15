35507f377d2ec347096ee4f586f8900c64e0a591
Ahmad Rosid Komarudin <arrosid@Ahmads-MacBook-Air.local>
2019-05-15 09:05:45 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,74 @@
+new Vue({
+    el: '#app',
+    
+    data: {
+        currencies: {},
+        amount: 0,
+        from: 'EUR',
+        to: 'USD',
+        result: 0,
+        loading: false
+    },
+    
+    mounted() {
+        this.getCurrencies()
+    },
+
+    computed: {
+        
+        formattedCurrencies() {
+            return Object.values(this.currencies)
+        },
+
+        calculateResult() {
+            return (Number(this.amount) * this.result).toFixed(3);
+        },
+
+        disabled() {
+            return this.amount === 0 || !this.amount || this.loading;
+        }
+
+    },
+
+    methods: {
+        
+        getCurrencies() {
+            const currencies = localStorage.getItem('currencies');
+
+            if (currencies) {
+                this.currencies = JSON.parse(currencies);
+
+                return;
+            }
+
+            axios.get('https://free.currconv.com/api/v7/currencies?apiKey=d410c54c8c9b12e7ba0a')
+            .then(response =>{
+                this.currencies = response.data.results;
+                localStorage.setItem('currencies', JSON.stringify(response.data.results));
+            })
+        },
+
+        convertCurrency() {
+            const key = `${this.from}_${this.to}`
+            this.loading = true;
+            axios.get(`https://free.currconv.com/api/v7/convert?q=${key}&apiKey=d410c54c8c9b12e7ba0a`)
+             .then((response) => {
+                 this.loading = false;
+                 console.log(response);
+                 this.result = response.data.results[key].val;
+             })
+        }
+    },
+
+    watch: {
+        from() { //ketika from yang ada di data diganti,
+            this.result = 0;
+        },
+
+        to() {
+            this.result = 0;
+        }
+    }
+
+
+})
